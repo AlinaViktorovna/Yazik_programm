1c6597b4bb479e2462f25b1065c636a4293b1a80
Alina Pateychuk <ali_nochka@inbox.ru>
2024-03-13 23:01:50 +0500

    add Attest3

---
+++ Attest3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 3: Задайте произвольный массив. 
+//Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int [] n = {5, 4, 8, 1, 7, 9, 6, 2, 0, 3};
+static void ArrayReverse(int[] n, int size)
+
+{
+    if (size <= n.Length)
+    {
+        Console.Write(n[n.Length - size]);
+        ArrayReverse(n, size + 1);
+    }
+}
+ArrayReverse(n, 1);
